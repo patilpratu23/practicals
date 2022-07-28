743b01a065b8c95653c09ddbc5eef4107fc7271d
patilpratu23 <108452330+patilpratu23@users.noreply.github.com>
2022-07-28 13:20:46 +0530

    Add files via upload

--- MainInterface/MainInterface.java
+++ MainInterface/MainInterface.java
@@ -1,1 +1,35 @@
+interface boy
+    {
+      final int b=6;
+      public void display();
+    }
+interface girl
+    {
+      final int c=9;
+      public void display1();
+    }
 
+
+class Value implements boy,girl
+  {
+            public void display()
+            {
+              System.out.println("boy's values is "+b);
+            }
+            public void display1()
+            {
+                System.out.println("girl's values is "+c);
+            }
+  }
+
+class MainInterface
+{
+    public static void main(String arg[])
+    {
+        Value V1=new Value();
+        V1.display();
+        V1.display1();
+
+    }
+	
+}
