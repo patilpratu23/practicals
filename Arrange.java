7ee48cfbf7c238a0f52704f39e951cc84165965c
patilpratu23 <108452330+patilpratu23@users.noreply.github.com>
2022-07-28 12:00:05 +0530

    Add files via upload

---
+++ Arrange.java
@@ -0,0 +1,16 @@
+public class Arrange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Asending order");
+        for(int i=31;i<=40;i++)
+        {
+          System.out.println(i);
+        }
+  System.out.println("Desending order");
+        for(int j=100;j>=91;j--)
+        {
+          System.out.println(j);
+        }
+               }
+}
