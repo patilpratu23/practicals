5e947886973042e60b71c8f89847748a26345e95
patilpratu23 <108452330+patilpratu23@users.noreply.github.com>
2022-07-29 16:20:20 +0530

    Add files via upload

--- Skip/Skip.java
+++ Skip/Skip.java
@@ -1,1 +1,25 @@
-
+import java.io.FileInputStream;
+import java.io.IOException;
+public class Skip   
+{
+  public static void main(String[] args) 
+   {
+     try 
+       {
+         FileInputStream s = new FileInputStream("Favorite things.txt");
+         int i = 0;
+         s.skip(5);
+         System.out.print("Printing text from index 3: ");
+         while ((i=s.read()) != -1) 
+          {
+            System.out.print((char)i);
+          }
+        
+         s.close();
+       }
+         catch (Exception e) 
+           {
+             System.out.println(e);
+           }
+   }
+}
